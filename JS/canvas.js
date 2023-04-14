422e3320def9e57b2f55925a5475b6996011e004
MrBacon470 <mrbacon470@outlook.com>
2023-04-14 11:19:42 -0500

    fix grid instantiation issue

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,6 +1,6 @@
 const canvasData = {
-    height: 800,
-    width: 800,
+    height: 640,
+    width: 640,
     pixelSize: 16,
 }
 
@@ -8,8 +8,6 @@ let canvas = document.getElementById('simulationCanvas')
 canvas.setAttribute('height',`${canvasData.height}px`)
 canvas.setAttribute('width',`${canvasData.width}px`)
 
-
-let size = 16;
 let canvas2D = canvas.getContext('2d')
 canvas2D.fillStyle = '#ff0000'
 canvas2D.fillRect(0,0,size,size)
--- JS/pixels.js
+++ JS/pixels.js
@@ -11,7 +11,10 @@ const pixelTypes = [
 
 let rows = canvasData.height/canvasData.pixelSize
 let cols = canvasData.width/canvasData.pixelSize
-let pixelGrid = new Array(rows).fill(new Array(cols).fill(0))
+let pixelGrid = new Array(rows)
+for(let i = 0; i < cols; i++) {
+    pixelGrid[i] = new Array(cols).fill(0)
+}
 console.log(`Rows: ${rows} | Columns: ${cols}`)
 console.log(pixelGrid)
 
@@ -32,5 +35,8 @@ function getPixelID(r,c) {
 }
 
 function drawPixel(r,c) {
-    
+    canvas2D.fillStyle = pixelTypes[pixelGrid[r][c]].color
+    canvas2D.fillRect(c*canvasData.pixelSize,r*canvasData.pixelSize,canvasData.pixelSize,canvasData.pixelSize)
 }
+drawPixel(0,0)
+drawPixel(5,0)
