ba98dab9451c6cdcd9e1370fc1849c16e3cd40c7
MrBacon470 <Mrbacon470@outlook.com>
2023-05-09 14:18:30 -0500

    importing files works

--- JS/data.js
+++ JS/data.js
@@ -4,18 +4,23 @@ let dataObject = {
     matrix: null,
 }
 
-function importData() {
-    const importedData = document.getElementById('importField').value
-    if(importedData <= 0 || importData === undefined) {
-        console.warn('Data Input is undefined or length 0')
-        return
-    }
+document.getElementById("fileImport").addEventListener("change", (event) => {
+    let fr = new FileReader();
+    const files = event.target.files
+    fr.onload = () => {
+        importData(fr.result)
+    };  
+    fr.readAsText(files[0]);
+});
+
+function importData(dataStr) {
     let dataObj = null
     try {
-        dataObj = Object.assign(dataObject,JSON.parse(importedData))
+        dataObj = Object.assign(dataObject,JSON.parse(dataStr))
     }
     catch(error) {
-        console.warn(`${error} in importData()`)
+        console.error(error)
+        return
     }
     for(let r = 0; r < dataObj.rows; r++) {
         for(let c = 0; c < dataObj.cols; c++) {
@@ -33,7 +38,7 @@ function exportData() {
     let exportedData = JSON.stringify(dataObject);
     const exportedDataText = document.createElement("textarea");
     const d = new Date()
-    downloadToFile(exportedData, `${d}.txt`, 'text/plain')
+    downloadToFile(exportedData, `ParticleSimData - ${d}.txt`, 'text/plain')
 }
 
 const downloadToFile = (content, filename, contentType) => {
