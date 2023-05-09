3a05a93b1028d0888fc8797d050336466fd5b564
MrBacon470 <mrbacon470@outlook.com>
2023-05-09 11:56:35 -0500

    Import Changes

--- JS/data.js
+++ JS/data.js
@@ -10,7 +10,13 @@ function importData() {
         console.warn('Data Input is undefined or length 0')
         return
     }
-    let dataObj = Object.assign(dataObject,JSON.parse(importedData))
+    let dataObj = null
+    try {
+        dataObj = Object.assign(dataObject,JSON.parse(importedData))
+    }
+    catch(error) {
+        console.warn(`${error} in importData()`)
+    }
     for(let r = 0; r < dataObj.rows; r++) {
         for(let c = 0; c < dataObj.cols; c++) {
             particleGrid[r][c] = dataObj.matrix[r][c]
