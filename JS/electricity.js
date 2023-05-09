e0b0eb37b300e6212cde2ba97acb59ce3b254bbe
MrBacon470 <Mrbacon470@outlook.com>
2023-05-09 14:39:23 -0500

    Electricity WORKS!!!!

--- JS/electricity.js
+++ JS/electricity.js
@@ -3,7 +3,7 @@
 function updateSPRK(r,c) {
     //SPRK can move 4 directions
     const currentParticle = getParticle(r,c)
-    if(currentParticle.sparked || !getParticleType(r,c).conductive){
+    if(currentParticle.sparked && !getParticleType(r,c).conductive){
         particleGrid[r][c].sparked = false
         particleGrid[r][c].tmp = null
         drawParticle(r,c)
@@ -48,60 +48,137 @@ function updateSPRK(r,c) {
             },200)
             break
         case 'Up':
-        case 'Down':
-            if(isInBounds(r + (currentParticle.tmp === 'Up' ? -1 : 1),c) && getParticleType(r + (currentParticle.tmp === 'Up' ? -1 : 1),c).conductive && !getParticle(r + (currentParticle.tmp === 'Up' ? -1 : 1),c).sparked) {
-                setParticleSparked(r + (currentParticle.tmp === 'Up' ? -1 : 1),c,true)
-                setParticleTmpVar(r + (currentParticle.tmp === 'Up' ? -1 : 1),c,currentParticle.tmp)
+            if(!isInBounds(r-1,c)) {
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
+                return
+            }
+            if(isInBounds(r-1,c) && getParticleType(r-1,c).conductive && !getParticle(r-1,c).sparked) {
+                setParticleSparked(r-1,c,true)
+                setParticleTmpVar(r-1,c,'Up')
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
             }
             else {
-                if(isInBounds(r + (currentParticle.tmp === 'Up' ? -1 : 1),c-1) && getParticleType(r + (currentParticle.tmp === 'Up' ? -1 : 1),c-1).conductive && !getParticle(r + (currentParticle.tmp === 'Up' ? -1 : 1),c-1).sparked) {
-                    setParticleSparked(r + (currentParticle.tmp === 'Up' ? -1 : 1),c-1,true)
-                    setParticleTmpVar(r + (currentParticle.tmp === 'Up' ? -1 : 1),c-1,currentParticle.tmp)
+                if(isInBounds(r,c-1) && getParticleType(r,c-1).conductive && !getParticle(r,c-1).sparked) {
+                    setParticleSparked(r,c-1,true)
+                    setParticleTmpVar(r,c-1,'Left')
                 }
-                if(isInBounds(r + (currentParticle.tmp === 'Up' ? -1 : 1),c+1) && getParticleType(r + (currentParticle.tmp === 'Up' ? -1 : 1),c+1).conductive && !getParticle(r + (currentParticle.tmp === 'Up' ? -1 : 1),c+1).sparked) {
-                    setParticleSparked(r + (currentParticle.tmp === 'Up' ? -1 : 1),c+1,true)
-                    setParticleTmpVar(r + (currentParticle.tmp === 'Up' ? -1 : 1),c+1,currentParticle.tmp)
+                else if(isInBounds(r,c+1) && getParticleType(r,c+1).conductive && !getParticle(r,c+1).sparked) {
+                    setParticleSparked(r,c+1,true)
+                    setParticleTmpVar(r,c+1,'Right')
+                }
+                else {
+                    setTimeout(()=>{
+                        setParticleSparked(r,c,false)
+                        setParticleTmpVar(r,c,null)
+                    },200)
                 }
             }
-            setTimeout(()=>{
-                setParticleSparked(r,c,false)
-                setParticleTmpVar(r,c,null)
-            },200)
+            break
+        case 'Down':
+            if(!isInBounds(r+1,c)) {
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
+                return
+            }
+            if(isInBounds(r+1,c) && getParticleType(r+1,c).conductive && !getParticle(r+1,c).sparked) {
+                setParticleSparked(r+1,c,true)
+                setParticleTmpVar(r+1,c,'Down')
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
+            }
+            else {
+                if(isInBounds(r,c-1) && getParticleType(r,c-1).conductive && !getParticle(r,c-1).sparked) {
+                    setParticleSparked(r,c-1,true)
+                    setParticleTmpVar(r,c-1,'Left')
+                }
+                else if(isInBounds(r,c+1) && getParticleType(r,c+1).conductive && !getParticle(r,c+1).sparked) {
+                    setParticleSparked(r,c+1,true)
+                    setParticleTmpVar(r,c+1,'Right')
+                }
+                else {
+                    setTimeout(()=>{
+                        setParticleSparked(r,c,false)
+                        setParticleTmpVar(r,c,null)
+                    },200)
+                }
+            }
+            
             break
         case 'Left':
-        case 'Right':
-            if(isInBounds(r,c + (currentParticle.tmp === 'Left' ? -1 : 1)) && getParticleType(r,c + (currentParticle.tmp === 'Left' ? -1 : 1)).conductive && !getParticle(r,c + (currentParticle.tmp === 'Left' ? -1 : 1)).sparked) {
-                setParticleSparked(r,c + (currentParticle.tmp === 'Left' ? -1 : 1),true)
-                setParticleTmpVar(r,c + (currentParticle.tmp === 'Left' ? -1 : 1),currentParticle.tmp)
+            if(!isInBounds(r,c-1)) {
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
+                return
+            }
+            if(isInBounds(r,c-1) && getParticleType(r,c-1).conductive && !getParticle(r,c-1).sparked) {
+                setParticleSparked(r,c-1,true)
+                setParticleTmpVar(r,c-1,'Left')
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
             }
             else {
-                if(isInBounds(r-1,c + (currentParticle.tmp === 'Left' ? -1 : 1)) && getParticleType(r-1,c + (currentParticle.tmp === 'Left' ? -1 : 1)).conductive && !getParticle(r-1,c + (currentParticle.tmp === 'Left' ? -1 : 1)).sparked) {
-                    setParticleSparked(r-1,c + (currentParticle.tmp === 'Left' ? -1 : 1),true)
-                    setParticleTmpVar(r-1,c + (currentParticle.tmp === 'Left' ? -1 : 1),currentParticle.tmp)
+                if(isInBounds(r+1,c) && getParticleType(r+1,c).conductive && !getParticle(r+1,c).sparked) {
+                    setParticleSparked(r+1,c,true)
+                    setParticleTmpVar(r+1,c,'Down')
                 }
-                if(isInBounds(r+1,c + (currentParticle.tmp === 'Left' ? -1 : 1)) && getParticleType(r+1,c + (currentParticle.tmp === 'Left' ? -1 : 1)).conductive && !getParticle(r+1,c + (currentParticle.tmp === 'Left' ? -1 : 1)).sparked) {
-                    setParticleSparked(r+1,c + (currentParticle.tmp === 'Left' ? -1 : 1),true)
-                    setParticleTmpVar(r+1,c + (currentParticle.tmp === 'Left' ? -1 : 1),currentParticle.tmp)
+                else if(isInBounds(r-1,c) && getParticleType(r-1,c).conductive && !getParticle(r-1,c).sparked) {
+                    setParticleSparked(r-1,c,true)
+                    setParticleTmpVar(r-1,c,'Up')
+                }
+                else {
+                    setTimeout(()=>{
+                        setParticleSparked(r,c,false)
+                        setParticleTmpVar(r,c,null)
+                    },200)
                 }
             }
-            setTimeout(()=>{
-                setParticleSparked(r,c,false)
-                setParticleTmpVar(r,c,null)
-            },200)
+            break
+        case 'Right':
+            if(!isInBounds(r,c+1)) {
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
+                return
+            }
+            if(isInBounds(r,c+1) && getParticleType(r,c+1).conductive && !getParticle(r,c+1).sparked) {
+                setParticleSparked(r,c+1,true)
+                setParticleTmpVar(r,c+1,'Right')
+                setTimeout(()=>{
+                    setParticleSparked(r,c,false)
+                    setParticleTmpVar(r,c,null)
+                },200)
+            }
+            else {
+                if(isInBounds(r+1,c) && getParticleType(r+1,c).conductive && !getParticle(r+1,c).sparked) {
+                    setParticleSparked(r+1,c,true)
+                    setParticleTmpVar(r+1,c,'Down')
+                }
+                else if(isInBounds(r-1,c) && getParticleType(r-1,c).conductive && !getParticle(r-1,c).sparked) {
+                    setParticleSparked(r-1,c,true)
+                    setParticleTmpVar(r-1,c,'Up')
+                }
+                else {
+                    setTimeout(()=>{
+                        setParticleSparked(r,c,false)
+                        setParticleTmpVar(r,c,null)
+                    },200)
+                }
+            }
             break
     }
-
-
-    setTimeout(()=>{
-        drawParticle(r,c)
-        if(isInBounds(r-1,c))
-            drawParticle(r-1,c)
-        if(isInBounds(r+1,c))
-            drawParticle(r+1,c)
-        if(isInBounds(r,c-1))
-            drawParticle(r,c-1)
-        if(isInBounds(r,c+1))
-            drawParticle(r,c+1)
-    },200)
-
 }
